40cafaf73b26b5f9949ce9139a7b8e8a9edc7ef2
kimboslice99 <94807745+kimboslice99@users.noreply.github.com>
2024-02-16 13:00:20 -0500

    Initial commit

---
+++ AbuseIPDBCacheComponent.cs
@@ -0,0 +1,239 @@
+﻿using RestSharp;
+using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace AbuseIPDBCacheComponent
+{
+    // Define a COM interface
+    [Guid("c1f9d247-e82d-4612-aa5b-ca3dde103a27")]
+    public interface AbuseIPDB
+    {
+        bool Block(string ip);
+        // all of the check items
+        bool IsSuccess();
+        bool IsFromCache();
+        string GetIpAddress();
+        bool IsPublic();
+        int GetIpVersion();
+        bool IsWhitelisted();
+        int GetAbuseConfidenceScore();
+        string GetCountryCode();
+        string GetCountryName();
+        string GetUsageType();
+        string GetISP();
+        string GetDomain();
+        bool IsTor();
+        int GetTotalReports();
+        int GetNumDistinctUsers();
+        DateTime GetLastReportedAt();
+        // set global variables
+        void SetMaxAgeInDays(int maxAgeInDays);
+        void SetApiKey(string apiKey);
+        void SetMaxConfidenceScore(int maxConfidenceScore);
+    }
+
+    // Define a class that implements the COM interface
+    [Guid("23456789-2345-2345-2345-234567890ABC")]
+    [ClassInterface(ClassInterfaceType.None)]
+    public class AbuseIPDBClient : AbuseIPDB
+    {
+        private int maxAgeInDays = 30; // Default value
+        private int maxConfidenceScore = 50;
+        private string apiKey = "";
+        private AbuseIpDbResponse response;
+
+        // initialize the database, not sure I should call initialization once here or on every Check call?
+        // for sake of speed, i figure this would be good
+        static AbuseIPDBClient()
+        {
+            DatabaseManager.InitializeDatabase();
+        }
+
+
+        public bool Block(string ip)
+        {
+            // Check if cached data exists and is not expired
+            if (DatabaseManager.TryGetCachedResponse(ip, out response))
+            {
+                response.data.isSuccess = true;
+                response.data.isFromCache = true;
+
+                if (response != null && response.data != null && response.data.abuseConfidenceScore < maxConfidenceScore)
+                    return false;
+                else
+                    return true;
+            }
+
+            using (RestClient client = new RestClient("https://api.abuseipdb.com/api/v2/"))
+            {
+                var request = new RestRequest("check", Method.Get);
+
+                request.AddHeader("Key", apiKey);
+                request.AddHeader("Accept", "application/json");
+                request.AddParameter("ipAddress", ip);
+                request.AddParameter("maxAgeInDays", maxAgeInDays);
+                var restResponse = client.Execute(request);
+                if (restResponse.IsSuccessful && restResponse.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    response = Newtonsoft.Json.JsonConvert.DeserializeObject<AbuseIpDbResponse>(restResponse.Content);
+                    response.data.isSuccess = true;
+
+                    // Cache the response
+                    DatabaseManager.CacheResponse(ip, response);
+
+                    if (response.data.abuseConfidenceScore < maxConfidenceScore)
+                        return false;
+                    else
+                        return true;
+                }
+                else
+                {
+                    Debug.WriteLine("response not successful " + restResponse.ErrorMessage + " " + restResponse.StatusCode);
+                    // Allow the client to connect if api failure code
+                    return false;
+                }
+            }
+        }
+
+
+
+        public bool IsFromCache()
+        {
+            return response?.data?.isFromCache ?? false;
+        }
+
+        public bool IsSuccess()
+        {
+            return response?.data?.isSuccess ?? false;
+        }
+
+        public string GetIpAddress()
+        {
+            return response?.data?.ipAddress;
+        }
+
+        public bool IsPublic()
+        {
+            return response?.data?.isPublic ?? false;
+        }
+
+        public int GetIpVersion()
+        {
+            return response?.data?.ipVersion ?? 0;
+        }
+
+        public bool IsWhitelisted()
+        {
+            return response?.data?.isWhitelisted ?? false;
+        }
+
+        public int GetAbuseConfidenceScore()
+        {
+            return response?.data?.abuseConfidenceScore ?? 0;
+        }
+
+        public string GetCountryCode()
+        {
+            return response?.data?.countryCode;
+        }
+
+        public string GetCountryName()
+        {
+            return response?.data?.countryName;
+        }
+
+        public string GetUsageType()
+        {
+            return response?.data?.usageType;
+        }
+
+        public string GetISP()
+        {
+            return response?.data?.isp;
+        }
+
+        public string GetDomain()
+        {
+            return response?.data?.domain;
+        }
+
+        public bool IsTor()
+        {
+            return response?.data?.isTor ?? false;
+        }
+
+        public int GetTotalReports()
+        {
+            return response?.data?.totalReports ?? 0;
+        }
+
+        public int GetNumDistinctUsers()
+        {
+            return response?.data?.numDistinctUsers ?? 0;
+        }
+
+        public DateTime GetLastReportedAt()
+        {
+            return response?.data?.lastReportedAt ?? DateTime.MinValue;
+        }
+
+        public void SetMaxAgeInDays(int maxAgeInDays)
+        {
+            this.maxAgeInDays = maxAgeInDays;
+        }
+
+        public void SetApiKey(string apiKey)
+        {
+            this.apiKey = apiKey;
+        }
+
+        public void SetMaxConfidenceScore(int maxConfidenceScore)
+        {
+            this.maxConfidenceScore = maxConfidenceScore;
+        }
+    }
+
+    // Define a custom class to deserialize JSON response
+    public class AbuseIpDbResponse
+    {
+        public Data data { get; set; }
+    }
+
+    public class Data
+    {
+        public bool isSuccess { get; set; }
+        public bool isFromCache { get; set; }
+        public string ipAddress { get; set; }
+        public bool isPublic { get; set; }
+        public int ipVersion { get; set; }
+        public bool? isWhitelisted { get; set; }
+        public int abuseConfidenceScore { get; set; }
+        public string countryCode { get; set; }
+        public string countryName { get; set; }
+        public string usageType { get; set; }
+        public string isp { get; set; }
+        public string domain { get; set; }
+        public bool? isTor { get; set; }
+        public int? totalReports { get; set; }
+        public int? numDistinctUsers { get; set; }
+        public DateTime? lastReportedAt { get; set; }
+        // Add more properties as needed for other data points
+    }
+
+    // Helper class for COM registration
+    public static class RegistrationHelper
+    {
+        public static void RegisterAssembly(System.Reflection.Assembly assembly, AssemblyRegistrationFlags flags)
+        {
+            RegistrationServices regsrv = new RegistrationServices();
+            regsrv.RegisterAssembly(assembly, flags);
+        }
+
+        public static void UnregisterAssembly(System.Reflection.Assembly assembly)
+        {
+            RegistrationServices regsrv = new RegistrationServices();
+            regsrv.UnregisterAssembly(assembly);
+        }
+    }
+}
---
+++ DatabaseManager.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Data.SQLite;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AbuseIPDBCacheComponent
+{
+    public class DatabaseManager
+    {
+        private static readonly object _lock = new object();
+
+        public static string ConnectionString
+        {
+            get
+            {
+                string Path = DatabasePath;
+                return $"Data Source={DatabasePath};Version=3;";
+            }
+        }
+
+        public static string DatabasePath
+        {
+            get
+            {
+                string dllPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+                string dllDirectory = Path.GetDirectoryName(dllPath);
+                return Path.Combine(dllDirectory, "AbuseIPDBCache.db");
+            }
+        }
+
+        public static void InitializeDatabase()
+        {
+            if (!File.Exists(DatabasePath))
+            {
+                SQLiteConnection.CreateFile(DatabasePath);
+                using (SQLiteConnection connection = new SQLiteConnection($"Data Source={DatabasePath};Version=3;"))
+                {
+                    connection.Open();
+                    string sql = @"CREATE TABLE IF NOT EXISTS CachedResponses (
+                            IpAddress TEXT PRIMARY KEY,
+                            IsPublic INTEGER,
+                            IpVersion INTEGER,
+                            IsWhitelisted INTEGER,
+                            AbuseConfidenceScore INTEGER,
+                            CountryCode TEXT,
+                            CountryName TEXT,
+                            UsageType TEXT,
+                            ISP TEXT,
+                            Domain TEXT,
+                            IsTor INTEGER,
+                            TotalReports INTEGER,
+                            NumDistinctUsers INTEGER,
+                            LastReportedAt TEXT,
+                            ExpirationDateTime TEXT
+                        )";
+
+                    using (SQLiteCommand command = new SQLiteCommand(sql, connection))
+                    {
+                        command.ExecuteNonQuery();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Cache the response from AbuseIPDB
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static async Task CacheResponse(string ip, AbuseIpDbResponse response)
+        {
+            await Task.Run(() =>
+            {
+                lock (_lock)
+                {
+                    using (SQLiteConnection connection = new SQLiteConnection(ConnectionString))
+                    {
+                        connection.Open();
+                        using (SQLiteCommand command = new SQLiteCommand(connection))
+                        {
+                            command.CommandText = @"INSERT OR REPLACE INTO CachedResponses (
+                                IpAddress,
+                                IsPublic,
+                                IpVersion,
+                                IsWhitelisted,
+                                AbuseConfidenceScore,
+                                CountryCode,
+                                CountryName,
+                                UsageType,
+                                ISP,
+                                Domain,
+                                IsTor,
+                                TotalReports,
+                                NumDistinctUsers,
+                                LastReportedAt,
+                                ExpirationDateTime) 
+                            VALUES (
+                                @IpAddress,
+                                @IsPublic,
+                                @IpVersion,
+                                @IsWhitelisted,
+                                @AbuseConfidenceScore,
+                                @CountryCode,
+                                @CountryName,
+                                @UsageType,
+                                @ISP,
+                                @Domain,
+                                @IsTor,
+                                @TotalReports,
+                                @NumDistinctUsers,
+                                @LastReportedAt,
+                                @ExpirationDateTime)";
+
+                            command.Parameters.AddWithValue("@IpAddress", ip);
+                            command.Parameters.AddWithValue("@IsPublic", response.data.isPublic);
+                            command.Parameters.AddWithValue("@IpVersion", response.data.ipVersion);
+                            command.Parameters.AddWithValue("@IsWhitelisted", response.data.isWhitelisted);
+                            command.Parameters.AddWithValue("@AbuseConfidenceScore", response.data.abuseConfidenceScore);
+                            command.Parameters.AddWithValue("@CountryCode", response.data.countryCode);
+                            command.Parameters.AddWithValue("@CountryName", response.data.countryName);
+                            command.Parameters.AddWithValue("@UsageType", response.data.usageType);
+                            command.Parameters.AddWithValue("@ISP", response.data.isp);
+                            command.Parameters.AddWithValue("@Domain", response.data.domain);
+                            command.Parameters.AddWithValue("@IsTor", response.data.isTor);
+                            command.Parameters.AddWithValue("@TotalReports", response.data.totalReports);
+                            command.Parameters.AddWithValue("@NumDistinctUsers", response.data.numDistinctUsers);
+                            command.Parameters.AddWithValue("@LastReportedAt", response.data.lastReportedAt);
+                            command.Parameters.AddWithValue("@ExpirationDateTime", DateTime.Now.AddHours(1));
+                            command.ExecuteNonQuery();
+                        }
+                    }
+                }
+            });
+        }
+
+
+        /// <summary>
+        /// Check if he have the data
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <param name="response"></param>
+        /// <returns></returns>
+        public static bool TryGetCachedResponse(string ip, out AbuseIpDbResponse response)
+        {
+            response = null;
+
+            using (SQLiteConnection connection = new SQLiteConnection(ConnectionString))
+            {
+                connection.Open();
+                using (SQLiteCommand command = new SQLiteCommand(connection))
+                {
+                    command.CommandText = "SELECT * FROM CachedResponses WHERE IpAddress = @IpAddress";
+                    command.Parameters.AddWithValue("@IpAddress", ip);
+                    using (SQLiteDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            DateTime expirationDateTime = reader.GetDateTime(reader.GetOrdinal("ExpirationDateTime"));
+                            if (expirationDateTime > DateTime.Now)
+                            {
+                                response = new AbuseIpDbResponse
+                                {
+                                    data = new Data
+                                    {
+                                        isFromCache = true,
+                                        isSuccess = true,
+                                        ipAddress = Convert.ToString(reader["IpAddress"]),
+                                        isPublic = Convert.ToBoolean(reader["IsPublic"]),
+                                        ipVersion = Convert.ToInt32(reader["IpVersion"]),
+                                        isWhitelisted = reader.IsDBNull(reader.GetOrdinal("IsWhitelisted")) ? false : Convert.ToBoolean(reader["IsWhitelisted"]),
+                                        abuseConfidenceScore = Convert.ToInt32(reader["AbuseConfidenceScore"]),
+                                        countryCode = Convert.ToString(reader["CountryCode"]),
+                                        countryName = Convert.ToString(reader["CountryName"]),
+                                        usageType = Convert.ToString(reader["UsageType"]),
+                                        isp = Convert.ToString(reader["ISP"]),
+                                        domain = Convert.ToString(reader["Domain"]),
+                                        isTor = reader.IsDBNull(reader.GetOrdinal("IsTor")) ? false : Convert.ToBoolean(reader["IsTor"]),
+                                        totalReports = reader.IsDBNull(reader.GetOrdinal("TotalReports")) ? 0 : Convert.ToInt32(reader["TotalReports"]),
+                                        numDistinctUsers = reader.IsDBNull(reader.GetOrdinal("NumDistinctUsers")) ? 0 : Convert.ToInt32(reader["NumDistinctUsers"]),
+                                        lastReportedAt = reader.IsDBNull(reader.GetOrdinal("LastReportedAt")) ? (DateTime?)null : Convert.ToDateTime(reader["LastReportedAt"])
+                                    }
+                                };
+                                return true;
+                            }
+                            else
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
